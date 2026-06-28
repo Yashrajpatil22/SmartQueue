6a72e589e436edc3df7410cded0c95abd8f2d39c
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-06-28 17:30:00 +0530

    Add QueueEntry model with schema definition

---
+++ backend/src/models/QueueEntry.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const queueEntrySchema = new mongoose.Schema(
+  {
+    CustomerName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tenantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tenant",
+      required: true,
+    },
+    queueId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Queue",
+        required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    tokenNumber: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    status: {
+      type: String,
+      enum: ["WAITING", "CALLED","SERVED", "CANCELLED","SKIPPED"],
+      default: "WAITING",
+    },
+    calledAt: {
+      type: Date,
+    },
+    servedAt: {
+      type: Date,
+    },
+    servedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const QueueEntry = mongoose.model("QueueEntry", queueEntrySchema);
+
+export default QueueEntry;
