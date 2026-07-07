5db9c4170162ca80d6b68a4b653d35b24a3ab147
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-07-07 17:50:53 +0530

    Refactor import statements for consistency and add welcome message handling in test HTML

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,49 @@
-import express from 'express';
-import connectDb from './db/index.js';
-import dotenv from 'dotenv';
-import http from 'http';
-import authRouter from './routes/Auth.route.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import staffRouter from './routes/Staff.route.js';
-import queueRouter from './routes/Queue.route.js';
-import { Server } from 'socket.io';
+import express from "express";
+import connectDb from "./db/index.js";
+import dotenv from "dotenv";
+import http from "http";
+import authRouter from "./routes/Auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import staffRouter from "./routes/Staff.route.js";
+import queueRouter from "./routes/Queue.route.js";
+import { Server } from "socket.io";
 
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server,{
-  cors:{
-    origin: "http://127.0.0.1:5500"
-  }
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+  },
 });
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use('/api/auth', authRouter);
-app.use('/api/staff', staffRouter);
-app.use('/api/queue', queueRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/staff", staffRouter);
+app.use("/api/queue", queueRouter);
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  // console.log(socket);
+  socket.emit("welcome", {
+    message: "Welcome to SmartQueue",
+  });
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-io.on('connection', (socket) => {
-  console.log('A user connected');
-});
 
 connectDb()
-    .then(() => {
-        server.listen(PORT, () => {
-          console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.log('Failed to connect to MongoDB', err);
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
