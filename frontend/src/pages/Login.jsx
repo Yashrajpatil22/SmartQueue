66aa6ca049092449669ee10082711971fe13036b
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-07-08 23:04:46 +0530

    feat: enhance Login page with form fields and styling

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   return (
-    <div>Login</div>
-  )
+    <div className="flex flex-col gap-6 items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-900">SmartQueue</h1>
+      <div className="p-8 border rounded-2xl w-96 shadow-lg bg-white border-gray-200">
+        <h2 className="text-2xl font-bold mb-5 text-center text-gray-900">
+          Login
+        </h2>
+        <form>
+          <div className="mb-5">
+            <label className="block mb-2 font-medium">Email</label>
+            <input
+              type="email"
+              className="w-full border rounded-lg px-3 py-2 focus:ring-2 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-5">
+            <label className="block mb-2 font-medium">Password</label>
+            <input
+              type="password"
+              className="w-full border rounded-lg px-3 py-2 focus:ring-2 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full cursor-pointer"
+            >
+              Login
+            </button>
+          </div>
+          <p className="mt-4 text-center text-gray-600">
+            Don't have an account?{" "}
+            <span
+              className="text-blue-500 hover:underline cursor-pointer"
+              onClick={() => navigate("/register")}
+            >
+              Register as Manager
+            </span>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
