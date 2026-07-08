c2a5878153e13693cc8fff00f01315665490fec9
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-07-08 22:29:45 +0530

    feat: add authentication context and login page, update dependencies

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, createContext, useContext } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  const checkAuth = async () => {
+    try{
+        const response = await axios.get('/api/auth/me',{withCredentials: true})
+        console.log(response)
+        setUser(response.data.user)
+    }catch(err){
+        setUser(null)
+    }finally{
+        setLoading(false)
+    }
+}
+
+const login = (userData) => {
+    setUser(userData);
+}
+
+const logout = () => {
+    setUser(null);
+}
+
+useEffect(() => {checkAuth()}, [])
+
+  return (
+    <AuthContext.Provider value={{ user, loading, checkAuth, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+
+
