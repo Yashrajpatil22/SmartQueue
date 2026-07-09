7c134f771170144e315ee768e0b480881a8e4cac
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-07-09 20:05:30 +0530

    feat: enhance QueueList page with API integration and improved UI layout

--- frontend/src/pages/QueueList.jsx
+++ frontend/src/pages/QueueList.jsx
@@ -3,16 +3,34 @@ import axios from 'axios'
 
 function QueueList() {
     const [queues, setQueues] = React.useState([]);
+    useEffect(() =>{
+        const fetchQueues = async () => {
+            try{
+                const response = await axios.get(
+                  `${import.meta.env.VITE_API_URL}/api/queue`,
+                  {
+                    withCredentials: true,
+                  }
+                );
+                setQueues(response.data.queues);
+            }catch(error){
+                console.log("Error fetching queues:", error)
+            }
+        }
+        fetchQueues();
+    }, [])
   return (
-    <div>
-        <h1>Queues</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className='text-3xl font-bold mb-4'>Queues</h1>
         {queues.map((queue) => (
-          <div key={queue.id}>
-            <h2>{queue.name}</h2>
-            <h2>{queue.status}</h2>
+          <div key={queue._id} className="bg-white p-4 shadow mb-4 w-1/2 border border-gray-300 rounded-2xl hover:border-blue-500 cursor-pointer">
+            <h2 className="text-xl font-semibold">{queue.name}</h2>
+            <h2 className="text-lg text-gray-600">{queue.status}</h2>
           </div>
         ))}
-        <button>Add Queue</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+          Add Queue
+        </button>
     </div>
   )
 }
