3d93f7540e62defc1a1fd37135ee498b1bbade23
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-07-16 23:54:18 +0530

    feat: add getQueueEntryStatus controller and route for fetching queue entry status

--- backend/src/controllers/Customer.controller.js
+++ backend/src/controllers/Customer.controller.js
@@ -2,17 +2,60 @@ import Queue from "../models/Queue.model.js";
 import QueueEntry from "../models/QueueEntry.model.js";
 import mongoose from "mongoose";
 import { getQueueUsingId } from "../utils/getQueue.js";
+import {
+  QUEUE_ENTRY_STATUS,
+  ACTIVE_QUEUE_STATUSES,
+  TERMINAL_QUEUE_STATUSES,
+} from "../constants/queueStatus.js";
 
 const getQueueDetails = async (req, res) => {
   const { queueId } = req.params;
-  try{
+  try {
     const queue = await (await getQueueUsingId(queueId)).populate("tenantId");
-    res.status(200).json({ queueName: queue.name, businessName: queue.tenantId.name });
-  }
-  catch(error){
+    res
+      .status(200)
+      .json({ queueName: queue.name, businessName: queue.tenantId.name });
+  } catch (error) {
     console.error("Error fetching queue details:", error);
     res.status(500).json({ message: "Failed to fetch queue details." });
   }
 };
 
-export { getQueueDetails };
+const getQueueEntryStatus = async (req, res) => {
+  const { entryId } = req.params;
+  try {
+    const entry = await QueueEntry.findById(entryId)
+      .populate("queueId")
+      .populate("tenantId");
+
+    if (!entry) {
+      return res.status(404).json({ message: "Queue entry not found." });
+    }
+    const customerAheadCount = await QueueEntry.countDocuments({
+      queueDate: entry.queueDate,
+      queueId: entry.queueId,
+      status: QUEUE_ENTRY_STATUS.WAITING,
+      tokenNumber: {
+        $lt: entry.tokenNumber,
+      },
+    });
+    res.status(200).json({
+      message: "Queue entry status fetched successfully.",
+      entry: {
+        tokenNumber: entry.tokenNumber,
+        status: entry.status,
+        customerAheadCount: customerAheadCount,
+        waitingTimeEstimate:
+          customerAheadCount * entry.queueId.averageServiceTime,
+        queueId: entry.queueId._id,
+        queueName: entry.queueId.name,
+        businessName: entry.tenantId.name,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching queue entry status:", error);
+    res.status(500).json({ message: "Failed to fetch queue entry status." });
+  }
+};
+
+export { getQueueDetails, getQueueEntryStatus };
--- backend/src/routes/Customer.route.js
+++ backend/src/routes/Customer.route.js
@@ -1,8 +1,9 @@
 import Route from "express";
-import { getQueueDetails } from "../controllers/Customer.controller.js";
+import { getQueueDetails, getQueueEntryStatus } from "../controllers/Customer.controller.js";
 
 const router = Route.Router();
 
 router.route("/:queueId").get(getQueueDetails);
+router.route("/entry/:entryId").get(getQueueEntryStatus);
 
 export default router;
